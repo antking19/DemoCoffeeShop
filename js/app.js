a3f7ef8909cbf28193c9ac6392c3e31172d28a1d
antking19 <cuong.truongkien@gmail.com>
2019-09-18 00:26:01 +0700

    fifth commit - end header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
-window.addEventListener('load', function(){
+eventListener();
+
+function eventListener(){
+    let ui = new UI;
+
+    window.addEventListener('load', function(){
+        ui.hidePreloader()
+    })
+    
+    document.querySelector('.navBtn').addEventListener('click', function(){
+        ui.showNav()
+    })
+
+    document.querySelector('.video__switch').addEventListener('click', function(){
+        ui.videoControls()
+    })
+}
+
+function UI(){}
+
+UI.prototype.hidePreloader = function(){
     document.querySelector('.preloader').style.display = "none";
-})
+}
+
+UI.prototype.showNav = function(){
+    document.querySelector('.nav').classList.toggle('nav--show')
+}
+
+UI.prototype.videoControls = function(){
+    let btn = document.querySelector('.video__switch-btn');
+    if(!btn.classList.contains('btnSlide')){
+        btn.classList.add('btnSlide');
+        document.querySelector('.video__item').pause();
+    }else{
+        btn.classList.remove('btnSlide');
+        document.querySelector('.video__item').play();
+    }
+}
